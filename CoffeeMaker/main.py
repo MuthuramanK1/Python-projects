8738f97d12855e3bc76c89e6c0d63665b3228810
MuthuramanK1 <megafree76@gmail.com>
2022-07-25 11:26:51 +0530

    from vscode

--- CoffeeMaker/main.py
+++ CoffeeMaker/main.py
@@ -5,29 +5,28 @@ from os import supports_effective_ids
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
+# this is the module that works wverything out
 
 menu = Menu()
 coffeemaker = CoffeeMaker()
 moneymachine = MoneyMachine()
 machine = True
 collection = 0
-while machine :
-    order = input("What would you like? " + menu.get_items() +' ').lower()
+while machine:
+    order = input("What would you like? " + menu.get_items() + ' ').lower()
     if order == "off":
         machine = False
     elif order == "report":
         coffeemaker.report()
         moneymachine.report()
-    else :
+    else:
         order_items = menu.find_drink(order)
         if order_items != None:
             is_sufficient = coffeemaker.is_resource_sufficient(order_items)
-            if is_sufficient :
+            if is_sufficient:
                 cost = order_items.cost
                 payment = moneymachine.make_payment(cost)
-                if payment :
+                if payment:
                     coffeemaker.make_coffee(order_items)
             else:
                 print("")
-        
